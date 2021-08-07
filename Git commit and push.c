33324cacfcb55c5e39d6de069f4edd41833f5984
Mahbub-Ferdous <mahbubferdous14@gmail.com>
2021-08-07 22:25:56 +0600

    Older file

---
+++ Git commit and push.c
@@ -0,0 +1,70 @@
+/*First of all create a gitInit file in your desktop file
+
+ 
+Then all your push file move to gitInit file
+
+ 
+Then go to your github account and create a new repository
+
+
+Then open your ubuntu terminal for command
+
+ 
+Command == cd home (For go to gitInit file)
+
+
+then command == ls
+
+  
+Now you see some your desktop saving file
+
+
+Then command this file which we need == cd gitInit
+
+
+Then Command again == ls
+
+
+Now you see some file which was saved in gitInit
+
+
+Now we check == git status result will no repository, you get a fatl error!
+
+
+So we create a git repository for commit and push so we command == git init
+
+
+Then system output is initialize a empty repository
+
+
+Then we check again == git status which output is no commit yet
+
+ 
+Now we command for add == git add -A
+
+
+Then we check again git status == git status which out show us the file color is green which is already added
+
+
+So now we ready to commit so which command we need == git commit -m "a massage"
+
+
+Again we check == git status then we will see nothing to commit, working tree clean 
+
+
+Now we ready to getiing push the coding so we go to our repository and copy our repository link
+
+
+Then we command == git remote add origin *paste the copy link*
+
+
+Now we command push == git push
+
+ 
+then again command == git push --set-upstream origin master 
+
+
+then system took your guthub id and pass 
+
+
+well done dude!!!!*/ 
